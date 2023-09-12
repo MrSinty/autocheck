ca9c82f3d0ae8b564049598bb27be15f5001bf8b
Кузьмин Дмитрий Андреевич <d.kuzmin@nicetu.spb.ru>
2023-09-13 01:27:20 +0300

    Made script to normally autocheck ver.1

--- autocheck.py
+++ autocheck.py
@@ -1,5 +1,3 @@
-import time
-
 from flask import Flask, request
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
@@ -9,6 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+#cron schedule 0 5/2 * * 1-6
 
 app = Flask(__name__)
 driver = None
@@ -19,10 +18,47 @@ def download_selenium():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-smh-usage")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-    #driver.get("https://digital.etu.ru/attendance/student")
-    driver.get("https://google.com")
-    title = driver.title
-    return title
+    if (not driver):
+        return "Failed to load Chrome Driver"
+    
+    driver.get("https://digital.etu.ru/attendance/student")
+    result = clicking_button()
+    driver.close()
+    return result
+
+def clicking_button():
+    element = None
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div/div/div[2]/p/div/button'))
+        )
+    finally:
+        element.click()
+
+    driver.implicitly_wait(1)
+    element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/form/div[1]/div/div/input")
+    element.send_keys("mrsinty@gmail.com")
+    element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/form/div[2]/div/div/input")
+    element.send_keys("Irjkf2345")
+    element.send_keys(Keys.RETURN)
+    if (not element):
+        return "Failed to enter email or password (maybe there's no such element)"
+
+    driver.implicitly_wait(5)
+
+    element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/div[4]/div/div[2]/form/button")
+    if (element):
+        element.click()
+    else:
+        return "Failed to press 'Авторизоваться' button"
+
+    element = driver.find_element(By.XPATH, 'XPATH //*[@id="__BVID__47"]/div/div/div[1]/div/div/button')
+    if (element):
+        element.click()
+    else:
+        return "Failed to press 'Посетил' button"
+
+    return "Success!"
 
 @app.route('/', methods=['GET','POST'])
 def home():
@@ -30,34 +66,12 @@ def home():
         return download_selenium()
     #elif(request.method == 'POST'):
 
-
 if (__name__ == "__main__"):
     app.run(debug=True, port=3000)
 
 
-
 #driver = webdriver.Chrome()
 #driver.get("https://digital.etu.ru/attendance/student")
-# element = None
-# try:
-#     element = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div/div/div[2]/p/div/button'))
-#     )
-# finally:
-#     element.click()
-
-# driver.implicitly_wait(1)
-# element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/form/div[1]/div/div/input")
-# element.send_keys("mrsinty@gmail.com")
-# element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/form/div[2]/div/div/input")
-# element.send_keys("Irjkf2345")
-# element.send_keys(Keys.RETURN)
-
-# driver.implicitly_wait(1)
-# element = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[2]/div/div[4]/div/div[2]/form/button")
-# element.click()
-
-# driver.implicitly_wait(1)
 
 #CSS_SELECTOR "#__BVID__46 > div > div > div:nth-child(1) > div > div > button"
 #XPATH //*[@id="__BVID__47"]/div/div/div[1]/div/div/button
@@ -73,8 +87,5 @@ if (__name__ == "__main__"):
 #action.click()
 #action.perform()
 #print(element)
-# time.sleep(30)
+#time.sleep(30)
 #driver.implicitly_wait(10)
-# print("Success")
-
-# driver.close()
